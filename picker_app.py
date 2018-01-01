270295c1c0520ffd11fa3a72d73aa04874b224dd
kruemel <weihek@gmail.com>
2018-01-01 17:45:58 +0100

    Added little database script to test postgresql

--- picker_app.py
+++ picker_app.py
@@ -1,7 +1,31 @@
 from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import ARRAY
 from boardgamegeek import BGGClient, exceptions
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/gamepicker'
+db = SQLAlchemy(app)
+
+# db Models
+class Game(db.Model):
+    gid = db.Column(db.Integer, primary_key=True)
+    name_collection = db.Column(db.String(100), nullable=False)
+    name_en = db.Column(db.String(100), nullable=False)
+    max_players = db.Column(db.Integer)
+    min_players = db.Column(db.Integer)
+    max_playing_time = db.Column(db.Integer)
+    average_weight = db.Column(db.Float)
+    best_playnum = db.Column(ARRAY(db.Integer))
+
+class GameQuery(db.Model):
+    qid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+    playnum = db.Column(db.Integer)
+    max_playtime = db.Column(db.Integer)
+    weight = db.Column(db.Integer)
+
 
 @app.route('/')
 def index():
@@ -20,6 +44,7 @@ def process():
         except:
             return jsonify({'error' : 'Oops! An error occured. Most likely I could not find this username..'})
     return jsonify({'error' : 'Missing data!'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
